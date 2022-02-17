112f9f3dac292654c750d29080980f842d11006d
KandpalPrachi <prachikandpal35@gmail.com>
2022-02-17 10:26:18 +0530

    This is the two_fer program from exercism

---
+++ two_fer.py
@@ -0,0 +1,9 @@
+def two_fer(name):
+    if type(name) is str:
+        return "One for " + name + ", one for me."
+    else:
+        return "One for you, one for me."
+        print(two_Fer)
+        
+print (two_fer("Alice"))
+
