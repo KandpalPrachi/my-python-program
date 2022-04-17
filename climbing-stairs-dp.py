be95052bbb560f7619e26889a2598e55e81e82dd
Prachi Kandpal <96049235+KandpalPrachi@users.noreply.github.com>
2022-04-17 23:31:05 +0530

    leetcode dynamic programming question

---
+++ climbing-stairs-dp.py
@@ -0,0 +1,18 @@
+"""this is the solution of leetcode dynamic programming
+https://leetcode.com/problems/climbing-stairs/submissions/
+"""
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <=2 :
+            return n
+        
+        prev1 = 1
+        prev2 = 2
+        current = 0
+        
+        for i in range(2,n):
+            current = prev1 + prev2
+            prev1 = prev2
+            prev2 = current
+            
+        return current
